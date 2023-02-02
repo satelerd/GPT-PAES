c26bcb7ee4b34c6750a408bc412262b0b727a533
Daniel <satelerd@gmail.com>
2023-02-01 22:28:04 -0300

    get_prompt() funcionando

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# write the utf-8 encoding at the top of the file
+# -*- coding: utf-8 -*-
+
 import os
 import time
 import json
@@ -6,13 +9,30 @@ import requests
 
 # Variables
 # --------------------------
-
+page = 0
 
 # Functions
 # --------------------------
 def get_prompt(page):
     """Read the txt file and append it to the prompt"""
-    return
+    prompt = """Lo siguiente es la PAES, la prueba mas importante de transicion universitaria en chile. Tu rol como destacado profesor de lengua española y literatura es conseguir la mejor nota en la prueba de competencia lectora.
+
+La prueba conciste en un texto, seguido de 7 preguntas de seleccion multiple. al final de las preguntas, deberas responder 1  a 1 explicando porque elegiste esa opcion.
+###
+
+$$$
+
+###
+Las respuestas correctas son las siguientes:
+1.
+"""
+
+    with open('./PAES/lenguaje/0.txt', 'r', encoding='utf-8') as f:
+        prompt = prompt.replace('$$$', f.read())
+        # this is giving me the UnicodeDecodeError: 'charmap' codec can't decode byte 0x9d in position 2462: character maps to <undefined>
+        # I tried to change the encoding to utf-8 but it didn't work
+
+    return prompt
 
 
 def gpt3_call(prompt):
@@ -28,7 +48,9 @@ def get_corrections():
 # Main
 # --------------------------
 if __name__ == '__main__':
-    answers = gpt3_call(get_prompt(1))
-    corrections = get_corrections()
-    print(answers)
-    print(corrections)
+    prompt = get_prompt(page)
+    print(prompt)
+    # answers = gpt3_call(prompt)
+    # corrections = get_corrections()
+    # print(answers)
+    # print(corrections)
