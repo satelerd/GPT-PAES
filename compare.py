76d027da67b5a9c4fff635987c9b5d87e5cd3a5f
Daniel <satelerd@gmail.com>
2023-02-08 12:38:32 -0300

    estructura

--- compare.py
+++ compare.py
@@ -3,33 +3,44 @@
 import os
 import pandas as pd
 
-# Variables
-scores = {}
 
-os.chdir("./Resultados")
-files = os.listdir()
+def get_scores():
+    # Variables
+    scores = {}
 
-cont = 0
-for file in files:
-    if cont == 0:
+    os.chdir("./Resultados")
+    files = os.listdir()
+
+    cont = 0
+    for file in files:
+        if cont == 0:  # Skip the first folder
+            cont += 1
+            continue
+
+        prompt_version = file.split("_")[0]
+        if prompt_version not in scores:
+            scores[prompt_version] = []
+
+        correct = 0
+        incorrect = 0
+        df = pd.read_excel(file)
+        for i in range(len(df)):
+            if df.iloc[i, 0] == df.iloc[i, 1]:
+                correct += 1
+            else:
+                incorrect += 1
+
+        scores[prompt_version].append([correct, incorrect])
+
         cont += 1
-        continue
+    print(scores)
+    return scores
 
-    prompt_version = file.split("_")[0]
-    if prompt_version not in scores:
-        scores[prompt_version] = []
 
-    df = pd.read_excel(file)
-    # print how many correct and incorrect answers are there
-    correct = 0
-    incorrect = 0
-    for i in range(len(df)):
-        if df.iloc[i, 0] == df.iloc[i, 1]:
-            correct += 1
-        else:
-            incorrect += 1
+def create_xlsx(scores):
+    return
 
-    scores[prompt_version].append([correct, incorrect])
 
-    cont += 1
-print(scores)
+if __name__ == "__main__":
+    scores = get_scores()
+    create_xlsx(scores)
